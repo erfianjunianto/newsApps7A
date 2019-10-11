591c5b8bce09de096fe2179cd674f83c30ecce41
erfianjunianto <erfianjunianto90@gmail.com>
2019-10-11 09:34:59 +0700

    update notification page

--- src/page/Notification.js
+++ src/page/Notification.js
@@ -21,8 +21,47 @@ export default class Notification extends Component {
             </Button>
           </Right>
         </Header>
-        <Content>
-         
+        <Content padder>
+          
+            <Grid style={{marginTop:10, borderBottomColor:"#000000", borderBottomWidth:1}}>
+              <Col style={{height:50, width:50, alignSelf:"center"}}>
+                <Image source={require('../images/icon-bell.png')} style={{height: 50, width:null, flex:1}} />
+              </Col>
+              <Col style={{padding:10}}>
+                <H3>Judul Pesan atau Notifikasi</H3>
+                <Text>Headline pesan atau notifikasi</Text>
+              </Col>
+            </Grid>
+
+            <Grid style={{marginTop:10, borderBottomColor:"#000000", borderBottomWidth:1}}>
+              <Col style={{height:50, width:50, alignSelf:"center"}}>
+                <Image source={require('../images/icon-bell.png')} style={{height: 50, width:null, flex:1}} />
+              </Col>
+              <Col style={{padding:10}}>
+                <H3>Judul Pesan atau Notifikasi</H3>
+                <Text>Headline pesan atau notifikasi</Text>
+              </Col>
+            </Grid>
+
+            <Grid style={{marginTop:10, borderBottomColor:"#000000", borderBottomWidth:1}}>
+              <Col style={{height:50, width:50, alignSelf:"center"}}>
+                <Image source={require('../images/icon-bell.png')} style={{height: 50, width:null, flex:1}} />
+              </Col>
+              <Col style={{padding:10}}>
+                <H3>Judul Pesan atau Notifikasi</H3>
+                <Text>Headline pesan atau notifikasi</Text>
+              </Col>
+            </Grid>
+
+            <Grid style={{marginTop:10, borderBottomColor:"#000000", borderBottomWidth:1}}>
+              <Col style={{height:50, width:50, alignSelf:"center"}}>
+                <Image source={require('../images/icon-bell.png')} style={{height: 50, width:null, flex:1}} />
+              </Col>
+              <Col style={{padding:10}}>
+                <H3>Judul Pesan atau Notifikasi</H3>
+                <Text>Headline pesan atau notifikasi</Text>
+              </Col>
+            </Grid>
           
         </Content>
         <Footernav />
