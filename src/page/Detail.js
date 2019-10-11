a07185466520ce73188dbacd10ceb908cd1282a5
erfianjunianto <erfianjunianto90@gmail.com>
2019-10-11 09:08:04 +0700

    update detail

--- src/page/Detail.js
+++ src/page/Detail.js
@@ -30,7 +30,7 @@ export default class Detail extends Component {
           <View style={{margin:10}}>
             <H3>JUDUL BERITANYA</H3>
             <Text>
-            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem
+            Isi berita hari ini adalah sebagai berikut yaitu. Isi berita hari ini adalah sebagai berikut yaitu. Isi berita hari ini adalah sebagai berikut yaitu. Isi berita hari ini adalah sebagai berikut yaitu. Isi berita hari ini adalah sebagai berikut yaitu. Isi berita hari ini adalah sebagai berikut yaitu. Isi berita hari ini adalah sebagai berikut yaitu. 
             </Text>
           </View>
           
