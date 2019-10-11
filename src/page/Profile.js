154ec53e037e23a42b1d099de999733d56df3f3f
erfianjunianto <erfianjunianto90@gmail.com>
2019-10-11 09:21:39 +0700

    update profile page

--- src/page/Profile.js
+++ src/page/Profile.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { Image } from 'react-native';
-import { Container, Header, Left, Body, Right, Button, Icon, Title, Footer, FooterTab, Content, Text, Card, CardItem, View, Grid, Col, H3 } from 'native-base';
+import { Container, Header, Left, Body, Right, Button, Icon, Title, Footer, FooterTab, Content, Text, Card, CardItem, View, Grid, Col, H2 } from 'native-base';
 import Footernav from './Footernav';
 export default class Profile extends Component {
   render() {
@@ -22,7 +22,16 @@ export default class Profile extends Component {
           </Right>
         </Header>
         <Content>
-         
+         <Card style={{height:150, width:150, alignSelf:"center", borderRadius:100, marginTop:100}}>
+           <CardItem cardBody style={{borderRadius:150}}>
+              <Image source={require('../images/talin.jpeg')} style={{height: 150, width:null, flex:1, borderRadius:100}} />
+           </CardItem>
+         </Card>
+         <View style={{alignItems:"center", paddingTop:30}}>
+           <H2>ERFIAN JUNIANTO</H2>
+           <Text>TEKNIK INFORMATIKA</Text>
+           <Text>17.7A</Text>
+         </View>
           
         </Content>
         <Footernav />
